1a715cea36beac577e99fb0e491cb26eb14d1213
Ramon Schmidt Rocha <ramonroc@gmail.com>
2016-09-07 23:24:55 -0300

    DefaultUserRepository create method added

--- app/Domains/Users/DefaultUserRepository.php
+++ app/Domains/Users/DefaultUserRepository.php
@@ -8,15 +8,18 @@
 
 namespace AcademicDirectory\App\Domains\Users;
 
-use Artesaos\Warehouse\BaseRepository;
+use Artesaos\Warehouse\AbstractCrudRepository;
 
-class DefaultUserRepository extends BaseRepository
+class DefaultUserRepository extends AbstractCrudRepository
 {
 
     protected $modelClass = User::class;
 
     public function create($data)
     {
+        $user = parent::create($data);
+        $data['user_id'] = $user->id;
+        parent::update(Participant::class, $data);
     }
 
 }

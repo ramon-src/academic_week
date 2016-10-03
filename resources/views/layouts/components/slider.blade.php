a7e856eea929f9aff93d1f3a2595c456c1dc4c3b
ramon183 <ramonroc@gmail.com>
2016-10-02 23:21:19 -0300

    Adding images and edited slide resolved #126

--- resources/views/layouts/components/slider.blade.php
+++ resources/views/layouts/components/slider.blade.php
@@ -2,11 +2,37 @@
     <!-- Indicators -->
     <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner" role="listbox">
         <div class="item active">
             <img class="first-slide"
-                 src="imgs/global_tecnopuc.jpg"
+                 src="imgs/facin.jpg"
+                 alt="First slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>Fique ligado nos eventos!</h1>
+                    <p><a class="btn btn-lg btn-primary" href="{{url('/login')}}" role="button">
+                            <i class="fa fa-user"></i>LOGIN</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img class="first-slide"
+                 src="imgs/global.jpg"
+                 alt="First slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>Fique ligado nos eventos!</h1>
+                    <p><a class="btn btn-lg btn-primary" href="{{url('/login')}}" role="button">
+                            <i class="fa fa-user"></i>LOGIN</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img class="first-slide"
+                 src="imgs/global2.jpg"
                  alt="First slide">
             <div class="container">
                 <div class="carousel-caption">
